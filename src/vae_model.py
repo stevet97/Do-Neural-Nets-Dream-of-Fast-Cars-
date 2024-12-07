935b66014ac093a7078e4aabf8c93d810fcdaa74
StevesInfinityDrive <44930005+stevet97@users.noreply.github.com>
2024-12-07 12:11:34 +0000

    Update vae_model.py

--- src/vae_model.py
+++ src/vae_model.py
@@ -1,1 +1,71 @@
+# VAE Model Script
+
+```python
+# Define KLRegularizer class
+class KLRegularizer(tf.keras.layers.Layer):
+    def __init__(self, kl_tolerance, z_size):
+        super(KLRegularizer, self).__init__()
+        self.kl_tolerance = kl_tolerance
+        self.z_size = z_size
+
+    def call(self, inputs):
+        mu, logvar = inputs
+        kl_loss = -0.5 * tf.reduce_sum(1 + logvar - tf.square(mu) - tf.exp(logvar))
+        return tf.maximum(kl_loss, self.kl_tolerance)
+
+# Importing necessary layers
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Flatten, Dense, Reshape
+
+# Building the VAE model within a class
+class ConvVAE(object):
+    def __init__(self, z_size=32, batch_size=1, learning_rate=0.0001,
+                 kl_tolerance=0.5, is_training=False, reuse=False, gpu_mode=False):
+        self.z_size = z_size
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.kl_tolerance = kl_tolerance
+        self.is_training = is_training
+        self.reuse = reuse
+
+        # Build the model architecture
+        self.build_vae_model()
+
+    def build_vae_model(self):
+        # Encoder
+        self.inputs = tf.keras.Input(shape=(64, 64, 3))
+        h = Conv2D(32, 4, strides=2, activation='relu', name='enc_conv1')(self.inputs)
+        h = Conv2D(64, 4, strides=2, activation='relu', name='enc_conv2')(h)
+        h = Conv2D(128, 4, strides=2, activation='relu', name='enc_conv3')(h)
+        h = Conv2D(256, 4, strides=2, activation='relu', name='enc_conv4')(h)
+        h = Flatten()(h)
+
+        # Latent variables
+        self.mu = Dense(self.z_size, name='enc_fc_mu')(h)
+        self.logvar = Dense(self.z_size, name='enc_fc_logvar')(h)
+        self.sigma = tf.keras.layers.Lambda(lambda x: tf.exp(x / 2.0))(self.logvar)
+        self.epsilon = tf.keras.layers.Lambda(lambda x: tf.random.normal(tf.shape(x)))(self.mu)
+        self.z = tf.keras.layers.Add()([self.mu, tf.keras.layers.Multiply()([self.sigma, self.epsilon])])
+
+        # Decoder
+        h = Dense(1024, name='dec_fc')(self.z)
+        h = Reshape((1, 1, 1024))(h)
+        h = Conv2DTranspose(128, 5, strides=2, activation='relu', name='dec_deconv1')(h)
+        h = Conv2DTranspose(64, 5, strides=2, activation='relu', name='dec_deconv2')(h)
+        h = Conv2DTranspose(32, 6, strides=2, activation='relu', name='dec_deconv3')(h)
+        self.outputs = Conv2DTranspose(3, 6, strides=2, activation='sigmoid', name='dec_deconv4')(h)
+
+        # KL Divergence Regularization Layer
+        kl_layer = KLRegularizer(kl_tolerance=self.kl_tolerance, z_size=self.z_size)([self.mu, self.logvar])
+
+        # Create the model
+        self.vae = tf.keras.Model(self.inputs, self.outputs)
+
+    def compile_vae_model(self):
+        # Compile the Model
+        self.vae.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate),
+                         loss='mse')
+
+# Create an instance of ConvVAE and compile the model
+vae_model = ConvVAE()
+vae_model.compile_vae_model()
 
